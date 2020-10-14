e6e30693b328047ed3671f8591b6e486c50c5832
Trina Shih <trina.shih@gmail.com>
2020-10-14 10:23:26 -0700

    Updates

---
+++ precache-manifest.849e61c760849bca0b1349888907a816.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38ea318db482d476f0572a144d30f9e7",
+    "url": "/moviesearch/index.html"
+  },
+  {
+    "revision": "cb7154206db92d3cafe0",
+    "url": "/moviesearch/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "cad847560bb7aa676fbc",
+    "url": "/moviesearch/static/js/2.f75782ba.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/moviesearch/static/js/2.f75782ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb7154206db92d3cafe0",
+    "url": "/moviesearch/static/js/main.36624cc2.chunk.js"
+  },
+  {
+    "revision": "b5a767c26bfb1de4a3b5",
+    "url": "/moviesearch/static/js/runtime-main.aa432347.js"
+  }
+]);
